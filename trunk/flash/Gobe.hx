441088d80f87d88b5e528af00e3f4a8906657963
bpederse <bpederse@fc3a0877-4f34-0410-b212-ef1cdd1c6ea5>
2007-07-11 16:36:38 +0000

    remove pre so it doesnt force html

--- trunk/flash/Gobe.hx
+++ trunk/flash/Gobe.hx
@@ -86,8 +86,8 @@ class Gobe extends Sprite {
         for(pair in json){
             g.lineStyle(2);
             trace(pair.annotation);
-            rect.tf.text = "<pre>" + pair.annotation + "</pre>";
-            rect.tf.text += "\n<a target='_blank' href='" + pair.link + "'>full annotation</a>";
+            rect.tf.text = pair.annotation;
+            //rect.tf.text += "\n<a target='_blank' href='" + pair.link + "'>full annotation</a>";
             for(hsp in Reflect.fields(pair.features)){
                  
                 if(hsp == ''){ isGene = true; continue; }
