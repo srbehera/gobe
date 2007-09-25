5fd23a73367997949e0fb131690939bde7224874
bpederse <bpederse@fc3a0877-4f34-0410-b212-ef1cdd1c6ea5>
2007-09-25 21:59:07 +0000

    add new_images.sh to easily allow updating of testcase.

---
+++ trunk/new_images.sh
@@ -0,0 +1,18 @@
+if [ ! -f /opt/apache/CoGe/tmp/${1}.sqlite ]
+then
+    echo /opt/apache/CoGe/tmp/${1}.sqlite does not exist
+    exit;
+fi
+
+echo "adding new files to the directory"
+svn up /opt/apache/CoGe/gobe/
+svn rm /opt/apache/CoGe/gobe/tmpdir/*
+cp /opt/apache/CoGe/tmp/${1}*.png /opt/apache/CoGe/gobe/tmpdir/
+cp /opt/apache/CoGe/tmp/${1}.sqlite /opt/apache/CoGe/gobe/tmpdir/
+svn add /opt/apache/CoGe/gobe/tmpdir/*
+
+perl -p -i -e "s/GEv[^']+/$1/" /opt/apache/CoGe/gobe/index.html
+N=`ls /opt/apache/CoGe/tmp/${1}*.png | wc -l`
+echo $N
+perl -p -i -e "s/\('n'\)\s*\|\|\s*[^\)]+/('n') || ${N}/" /opt/apache/CoGe/gobe/index.html
+
