5a7bf9eadf154d10174bd93372fc1a4ff0529871
bpederse <bpederse@fc3a0877-4f34-0410-b212-ef1cdd1c6ea5>
2008-01-17 18:16:36 +0000

    use more general paths

--- trunk/predict_cns.py
+++ trunk/predict_cns.py
@@ -24,14 +24,12 @@ def find_colinear_hits(blastfile, qeval, seval, mask='query', as_str=False):
     # need these to convert the absolute coords in sqlite to match
     # the local positions in the fresh blast
     qmin, smin = [x[0] for x in cur.execute('SELECT bpmin FROM image_info ORDER BY id')]
-    print >>sys.stderr, "COLINEAR:" + str(qmin)
 
     # so we mask the new blast with anything that's NOT an HSP
     sql = "SELECT image_id, bpmin, bpmax FROM image_data WHERE type != 'HSP'"
     if mask == 'query': sql += ' AND image_id = 1'
 
     b = blast_array(blastfile, dopickle=False, best_hit=0, maxkeep=99999)
-    print >>sys.stderr, "BLAST_ARRAY:" + str(b)
     if plot:
         pylab.plot(b['qstart'], b['sstart'], "kx")
     b = b[(b['eval'] < qeval) & (b['eval'] < seval)]
@@ -82,16 +80,10 @@ def find_colinear_hits(blastfile, qeval, seval, mask='query', as_str=False):
     return cnss
 
 
-def predict(base_name, tmpdir=None):
-    if tmpdir is None:
-        tmpdir = '/opt/apache/CoGe/tmp/'
-        #if os.path.exists('/opt/apache/CoGe/gobe/tmp'):
-        #    tmpdir = '/opt/apache/CoGe/gobe/tmp/'
-     
-    log = tmpdir + '/' + base_name  + ".log"
+def predict(base_name, logfile=None):
     seen = 0
     bl2seq, qeval, seval = None, None, None
-    for line in open(log):
+    for line in open(logfile):
         if seen == 2: break
         line = line[:-1]
         if 'bl2seq' in line: bl2seq = line
@@ -111,7 +103,7 @@ def predict(base_name, tmpdir=None):
     print  >>sys.stderr, "%s | grep -v '#' > %s" % (bl2seq, blast_out)
     print >>sys.stderr, commands.getoutput("%s | grep -v '#' > %s" % (bl2seq, blast_out))
     predicted = find_colinear_hits(blast_out, qeval, seval)
-    print >>sys.stderr, predicted
+    print >>sys.stderr, "PREDICTED:", predicted
     # use tab-delimited output, and only the top strand
     return predicted
 
