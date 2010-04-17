91fbc57ab653d4eb75b4c56d256f2dc64cbcee3d
bpederse <bpederse@fc3a0877-4f34-0410-b212-ef1cdd1c6ea5>
2010-04-17 23:28:41 +0000

    update to drawing in flash.

---
+++ trunk/config.js
@@ -0,0 +1,11 @@
+{
+ "tracks": [
+     {"title": "At2g26540", "bpmin": 1234, "bpmax": 4567},
+     {"title": "At4g16240", "bpmin": 21274, "bpmax": 24567}
+     ],
+ "annotations": [
+     {"type": "CDS", "start": 1259, "end": 1467, "strand": "+", "track": 0, "name": "At2g26540"},
+     {"type": "CDS", "start": 1259, "end": 1467, "strand": "+", "track": 1, "name": "At4g16240"}
+ ],
+ "edge": [[123, 134, 0.6], [144, 171, 0.2]]
+}
