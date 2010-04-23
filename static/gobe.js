7e54ec8ba87bc82f0f13db3c46187c5f788a4efe
bpederse <bpederse@fc3a0877-4f34-0410-b212-ef1cdd1c6ea5>
2010-04-23 16:56:13 +0000

    cleanup stuff from old implementation.

--- static/gobe.js
+++ static/gobe.js
@@ -1,6 +1,9 @@
 var Gobe = {
-    WEDGE: 'wedge',
-    LINE: 'line',
+    'mouseover': function(id, name, px, py, bpx, bpy){
+    },
+    'click': function(id, name, px, py, bpx, bpy){
+    },
+
     DIV: 'flashcontent',
     'get_movie': function () {
           if (swfobject.ua.ie) {
@@ -9,27 +12,12 @@ var Gobe = {
               return document[Gobe.DIV];
           }
     },
-    'handle_html': function(html){
-        document.getElementById('querybox').innerHTML = html;
-    },
-    'set_genespace': function(updown, idx, value){
-        var eid = 'dr' + updown + idx;
-        document.getElementById(eid).value = value;
-    },
     'clear': function(){
         // dont change this!
         Gobe.swf.clear_wedges();
-    },       
+    },
     'set_linewidth': function(linewidth){
         // dont change this. could check for 0 <= lw <= ?
         Gobe.swf.set_linewidth(linewidth);
-    },
-    'set_connector': function(contype){
-        // dont change this!
-        contype = contype.toLowerCase();
-        if (! (contype == Gobe.WEDGE || contype == Gobe.LINE)){
-            alert('must be one of: ' + Gobe.WEDGE + ' or ' + Gobe.LINE);
-        }
-        Gobe.swf.set_connector(contype);
     }
 };
